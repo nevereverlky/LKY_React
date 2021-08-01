5ae825e1d08a37183229fe131d5e6d12196ebd5a
lky <genius_lky@163.com>
2021-08-01 23:46:37 +0800

    教程p37-p44,头部组件完成(三角形效果,获取当前时间及格式化,jsonp请求百度天气预报接口,withRouter包装非路由组件获取当前路由,退出登录清除保存的user,抽取通用的类链接按钮组件)

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,8 @@
 包含 n 个接口请求函数的模块 
 每个函数返回 promise 
 */ 
+// import { message } from 'antd'
+// import jsonp from 'jsonp'
 import ajax from './ajax'
 
 // 登陆
@@ -11,3 +13,26 @@ import ajax from './ajax'
 export const reqLogin = (username, password) => ajax('/login', {username, password}, 'POST')
 //添加用户
 export const reqAddUser = (user) => ajax('/manage/user/add', user, 'POST')
+//jsonp请求的接口请求函数
+//这个api目前失效
+
+// export const reqWeather = (city) => {
+
+//     return new Promise((resolve,reject) => {
+//         const url = 'http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2'
+//         // 发送jsonp请求
+//         jsonp(url, {}, (err, data) => {
+//             console.log('jsonp()', err, data)
+//             //如果成功了
+//             if(!err && data.status==='success') {
+//                 //取出需要的数据
+//                 const {dayPictureUrl, weather} = data.results[0].weather_data[0]
+//                 resolve({dayPictureUrl, weather})
+//             } else {
+//             //如果失败了
+//             message.error('获取天气信息失败')
+//             }
+//         })
+//     })
+// }
+// reqWeather('北京')
